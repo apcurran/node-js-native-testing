84593c04e83262ea24f23a6d09798c3845e5a541
Alex Curran <alexcurran.va@gmail.com>
2024-01-16 15:50:00 -0600

    Base setup with utils random int func

---
+++ utils/get-random-int-inclusive.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number}
+ */
+function getRandomIntInclusive(min, max) {
+    const minValue = Math.ceil(min);
+    const maxValue = Math.floor(max);
+
+    return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+}
